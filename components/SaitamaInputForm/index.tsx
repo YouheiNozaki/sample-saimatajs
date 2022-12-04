5b735b3cbb1e00736c37298dceac625ebef0ffac
Youhei Nozaki <youhey.hey0505@gmail.com>
2022-12-05 00:29:17 +0900

    add label

--- components/SaitamaInputForm/index.tsx
+++ components/SaitamaInputForm/index.tsx
@@ -2,6 +2,7 @@ import { InputWithAlert } from '../InputWithAlert';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
+import styles from './saitamaInputForm.module.css';
 
 const schema = z.object({
   name: z.string(),
@@ -24,8 +25,12 @@ export const SaitamaInputForm = () => {
         console.log(d);
       })}
     >
+      <label htmlFor="name" className={styles.label}>
+        行ったことがある埼玉県の市町村は？
+      </label>
       <InputWithAlert
-        inputProps={{ ...register('name') }}
+        // aria-invalid={errors.name ? 'true' : 'false'}
+        inputProps={{ ...register('name'), id: 'name' }}
         errorMessage={errors.name && errors.name.message}
       />
     </form>
